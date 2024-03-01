0a464f3ada9a3820b477502851a9595241e0ab18
jackson-peddle <161519795+jackson-peddle@users.noreply.github.com>
2024-03-01 14:11:36 -0600

    Update sketch.js
    started on interactive scene

--- scene/sketch.js
+++ scene/sketch.js
@@ -1,12 +1,30 @@
-
+let scene = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   rectMode(CENTER);
   background("white");
+  track();
 }
 
 function draw() {
-  background(220);
+}
+
+//making the track
+function track(){
+  background("green");
+  fill(220, 149, 18);
+  rect(windowWidth/2, windowHeight/2, windowWidth/1.5, windowHeight/1.1, 10);
+  fill(0);
+  circle(windowWidth/2, windowHeight/2, windowHeight/1.2);
+  fill("red");
+  circle(windowWidth/2, windowHeight/2, windowHeight/2.5)
+
+
+
+}
+
+function title(){
+
 }
