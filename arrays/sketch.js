757bc5938aae5b3a0ba0b4a16fd4b7112d337207
jackson-peddle <161519795+jackson-peddle@users.noreply.github.com>
2024-03-22 14:10:25 -0600

    used lists for stuffs

--- arrays/sketch.js
+++ arrays/sketch.js
@@ -3,35 +3,66 @@
 // 04/08/24
 
 
-
+let dx, dy;
 let x;
 let y;
-let d = 100;
+let d;
 let time = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  y = height/2;
 }
 
 function draw() {
   background(220);
-  x = noise(time) * width;
-  y = noise(time + 1000) * height;
-  fill("black");
-  noStroke();
-  circle(x, y, 50);
-  time += 0.01;
-  ball(x, y, d);
-  ball(x+100, y+100, d);
+  ball();
 }
 
-function ball(x, y, d){
-  x = noise(time) * width;
-  y = noise(time + 1000) * height;
+function ball(){
+  let perlinBall = {
+    x: noise(time) * width,
+    y: noise(time + 1000) * height,
+    d: windowHeight/5,
+  };
   fill("black");
   noStroke();
-  circle(x, y, d);
+  circle(perlinBall.x, perlinBall.y, perlinBall.d);
   time += 0.01;
+
+  let playerBall = {
+    x: width/2,
+    y: height/2,
+    d: windowHeight/20,
+    dx: 10,
+    dy: 10,
+  };
+
+  fill("red");
+  circle(playerBall.x, playerBall.y, playerBall.d);
+  noStroke();
+
+  if (playerBall.y < height - playerBall.d) {
+    if (keyIsDown(83)) {
+      //s
+      playerBall.y += playerBall.dy;
+    }
+  }
+  if (playerBall.y > playerBall.d) {
+    if (keyIsDown(87)) {
+      //w
+      playerBall.y -= playerBall.dy;
+    }
+  }
+  if (playerBall.x > playerBall.d) {
+    if (keyIsDown(65)) {
+      //a
+      playerBall.x -= playerBall.dx;
+    }
+  }
+  if (playerBall.x < width - playerBall.d) {
+    if (keyIsDown(68)) {
+      //d
+      playerBall.x += playerBall.dx;
+    }
+  }
 }
